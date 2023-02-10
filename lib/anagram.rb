d440c6fd6343e5ed6a78d8dfd46d1b3ffed2c4b9
Osorabrian <osorabrian@gmail.com>
2023-02-10 22:46:33 +0300

    completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Anagram
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(arr)
+        new_array = Array.new
+        arr.each do |item|
+            if item.chars.sort == @word.chars.sort
+               new_array << item
+            end
+        end
+        return new_array
+    end
+
+end
+
+new_word = Anagram.new('listen')
+puts new_word.match(%w[enlists google inlets banana])
